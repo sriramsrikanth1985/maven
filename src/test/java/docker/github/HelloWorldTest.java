fcd3edfa7dfa8abd9263e1eda53ff3702347aaca
esrirsr <esrirsr@IN-00223280.ericsson.se>
2019-10-02 20:46:02 +0530

    Modified the HelloWorldTest.java

--- src/test/java/docker/github/HelloWorldTest.java
+++ src/test/java/docker/github/HelloWorldTest.java
@@ -6,6 +6,6 @@ public class HelloWorldTest extends TestCase {
 	public void testPrintHello(){
         System.out.println("HelloWorldTest");
         GreaterNum gn = new GreaterNum();
-        assertEquals("Hello World Srikanth !!", HelloWorld.printHello());
+        assertEquals("Hello World !!", HelloWorld.printHello());
 	}
 }
